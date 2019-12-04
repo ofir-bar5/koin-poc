39046417052ce62e77fe3ce3fb0b996b7c8590e1
Ofir <ofirbar5@gmail.com>
2019-12-04 11:29:41 +0200

    set up the buttons

--- app/src/main/java/com/bar/koinpoc/MainActivity.kt
+++ app/src/main/java/com/bar/koinpoc/MainActivity.kt
@@ -2,12 +2,26 @@ package com.bar.koinpoc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_sharedprefs.setOnClickListener {
+            Toast.makeText(this, "shared prefs", Toast.LENGTH_SHORT).show()
+        }
+
+        btn_savetodb.setOnClickListener {
+            Toast.makeText(this, "save to db", Toast.LENGTH_SHORT).show()
+        }
+
+        btn_network.setOnClickListener {
+            Toast.makeText(this, "network", Toast.LENGTH_SHORT).show()
+        }
 
 
 
